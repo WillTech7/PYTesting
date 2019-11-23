555d8cac81de1f446805df71d8c9a03f2c80bac2
BuildTools <unconfigured@null.spigotmc.org>
2019-11-22 19:26:48 -0600

    helloworld

--- venv/helloworld.py
+++ venv/helloworld.py
@@ -1,6 +1,6 @@
 import time
 print ("hello world")
-print("This is a test of the emergency broadcast system.")
+print("This is a test of the emergency broadcast system. ")
 for i in "TESTING":
     print (i)
     time.sleep(.5)
